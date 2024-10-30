edad42412f838d6866cc956cf187e5c500d0fa98
ST10107850 <ST10107850@rcconnect.edu.za>
2024-10-30 12:01:45 +0200

    Fixing my main

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { AuthProvider } from "./context/AuthContext.tsx";
 import App from "./App.tsx";
-import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <AuthProvider>
